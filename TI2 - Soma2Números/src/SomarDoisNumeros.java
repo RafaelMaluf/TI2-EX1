eb110804e884ca41ec8630ec562e9d8275e308d8
RafaelMaluf <104031822+RafaelMaluf@users.noreply.github.com>
2023-03-02 16:36:07 -0300

    Add files via upload

---
+++ TI2 - Soma2Números/src/SomarDoisNumeros.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class SomarDoisNumeros {
+public static Scanner sc = new Scanner(System.in);
+public static void main (String args[]) {
+//Declaração de variaveis
+int num1, num2, soma;
+//le os números a serem somados
+System.out.println("Digite um numero");
+num1 = sc.nextInt();
+System.out.println("Digite outro numero");
+num2 = sc.nextInt();
+//soma
+soma = num1 + num2;
+//printa na tela
+System.out.println("Soma: "+ soma);
+}
+}
